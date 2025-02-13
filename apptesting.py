1ff5a41bceeed50abe3d5133b5e4cbb1c33165a4
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2025-02-13 11:33:59 +0530

    Update apptesting.py

--- apptesting.py
+++ apptesting.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from pathlib import Path
 
 # Load product data and Party data
-biolume_df = pd.read_csv('MKT+Biolume - Inventory System - Invoice (2).csv')
+biolume_df = pd.read_csv('DB Allgen Trading - Data.csv')
 party_df = pd.read_csv('MKT+Biolume - Inventory System - Party (2).csv')
 
 # Set the title and favicon
