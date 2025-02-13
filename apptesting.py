d803de8252df1a201aa8f7f32555e5cc132ab2ae
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2025-02-13 18:22:57 +0530

    Update apptesting.py

--- apptesting.py
+++ apptesting.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Set the title and favicon
 st.set_page_config(
-    page_title="Inventory Tracker",
+    page_title="Biolume: ALLGEN TRADING Inventory System",
     page_icon=":shopping_bags:",
 )
 
